9fb767e073636d93119f009fbeaba7395488b2f2
Jose Alzate <josealzate97@gmail.com>
2026-02-03 17:13:07 -0500

    feat: Cambiar los campos consecutivos a tipo string en la tabla de órdenes

---
+++ database/migrations/2026_02_03_000002_change_consecutive_fields_to_string_in_order_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('order', function (Blueprint $table) {
+            $table->string('consecutive_serial', 9)->nullable()->change();
+            $table->string('consecutive_number', 9)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('order', function (Blueprint $table) {
+            $table->integer('consecutive_serial')->nullable()->change();
+            $table->integer('consecutive_number')->nullable()->change();
+        });
+    }
+};
